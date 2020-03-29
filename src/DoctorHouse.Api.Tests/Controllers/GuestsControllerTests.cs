72f4c4a7053907204aa9eb9b97eff109b01a2143
Carlos Ramirez <calbertora@gmail.com>
2020-03-29 11:15:16 -0500

    Add the Put with no session Guest Controller test method

--- src/DoctorHouse.Api.Tests/Controllers/GuestsControllerTests.cs
+++ src/DoctorHouse.Api.Tests/Controllers/GuestsControllerTests.cs
@@ -15,5 +15,21 @@ namespace DoctorHouse.Api.Tests.Controllers
 
             this.Assert401(response.Response);
         }
+
+        [Test]
+        public async Task Put_NoSession_401()
+        {
+            var guest = new GuestModel
+            {
+                JobAddress = "Test Job Address",
+                JobPlace = "Test Job Place",
+                Name = "Test Guest Name",
+                Phone = "555-555555"
+            };
+
+            var response = await this.PutAsync<GuestModel>($"/api/v1/requests/2/guests/2", guest);
+
+            this.Assert401(response.Response);
+        }
     }
 }
