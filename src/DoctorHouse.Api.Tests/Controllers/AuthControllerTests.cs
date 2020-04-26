064af401cb6287ababaa5f19701cfbb1ef2c3025
betocastillo86 <castillopradagabriel@gmail.com>
2020-04-26 09:34:57 -0500

    Added endpoint for current user

---
+++ src/DoctorHouse.Api.Tests/Controllers/AuthControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using DoctorHouse.Api.Models;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace DoctorHouse.Api.Tests.Controllers
+{
+    [TestFixture]
+    public class AuthControllerTests : BaseControllerTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public async Task Get_NoSession_401()
+        {
+            var response = await this.GetAsync<UserModel>($"/api/v1/auth/current");
+            this.Assert401(response.Response);
+        }
+
+        [Test]
+        public async Task Get_WithSession_SameUserAnd200()
+        {
+            var response = await this.GetAsync<UserModel>($"/api/v1/auth/current", defaultAuthentication: true);
+            this.Assert200(response.Response);
+            Assert.AreEqual(1, response.Content.Id);
+        }
+    }
+}
---
+++ src/DoctorHouse.Api/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Beto.Core.Exceptions;
+using Beto.Core.Web.Api.Controllers;
+using DoctorHouse.Api.Models;
+using DoctorHouse.Business.Security;
+using DoctorHouse.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorHouse.Api.Controllers
+{
+    [Route("api/v1/auth/current")]
+    public class AuthController : BaseApiController
+    {
+        private readonly IWorkContext workContext;
+
+        private readonly IMapper mapper;
+
+        public AuthController(
+            IMessageExceptionFinder messageExceptionFinder,
+            IWorkContext workContext,
+            IMapper mapper) : base(messageExceptionFinder)
+        {
+            this.workContext = workContext;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            var model = this.mapper.Map<User, UserModel>(this.workContext.CurrentUser);
+            return this.Ok(model);
+        }
+    }
+}
